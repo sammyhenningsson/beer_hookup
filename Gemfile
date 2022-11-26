1e6349b7dc650eae759661903c6455cf3745cd6e
Sammy Henningsson <sammy.henningsson@hey.com>
2022-11-26 21:12:23 +0100

    Add rspec

--- Gemfile
+++ Gemfile
@@ -60,4 +60,5 @@ group :test do
   gem "capybara"
   gem "selenium-webdriver"
   gem "webdrivers"
+  gem "rspec-rails"
 end
--- config/application.rb
+++ config/application.rb
@@ -30,5 +30,6 @@ module BeerHookup
     #
     # config.time_zone = "Central Time (US & Canada)"
     # config.eager_load_paths << Rails.root.join("extras")
+    config.generators.test_framework :rspec
   end
 end
