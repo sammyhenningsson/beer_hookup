72807a84450b86949ead32e8483c43bcca6d12b1
Sammy Henningsson <sammy.henningsson@hey.com>
2022-11-27 23:53:00 +0100

    Add recent searches

--- app/controllers/beers_controller.rb
+++ app/controllers/beers_controller.rb
@@ -1,7 +1,10 @@
 class BeersController < ApplicationController
   def index
     query = params[:query]
+    @recent_searches = recent_searches
     return render "start" unless query
+
+    save_search
 
     @beers = FetchBeers.call(query:, page:)
     render "empty_search_result" if @beers.blank?
@@ -9,6 +12,7 @@ class BeersController < ApplicationController
 
   def show
     @beer = FetchBeer.call(external_id: params[:id])
+    @recent_searches = recent_searches
   rescue PunkApi::Client::NotFoundError => error
     Rails.logger.error(error.message)
     render "not_found"
@@ -18,5 +22,18 @@ class BeersController < ApplicationController
 
   def page
     params[:page] || 1
+  end
+
+  def save_search
+    query = params[:query]
+    return if query.blank?
+
+    searches = recent_searches
+    searches.unshift query unless searches.include? query
+    session[:recent_searches] = searches[..4]
+  end
+
+  def recent_searches
+    session[:recent_searches]&.dup || []
   end
 end
